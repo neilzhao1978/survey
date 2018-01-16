b5b77017b89ac53b0ba81532cc03d72e9a6ac592
neil <12590578@qq.com>
2018-01-16 22:29:13 +0800

    add

---
+++ src/main/resources/init.sql
@@ -0,0 +1,3 @@
+create or replace view SERVEY_IMAGE as SELECT a.SURVEY_ID,a.ANSWER_ID,im.IMAGE_ID,im.IMAGE_NAME,im.IMAGE_DESC FROM ANSWER a left join ANSWER_IMAGE i  on a.ANSWER_ID =i.ANSWER_ID  left join IMAGE im on i.Image_id = im.IMAGE_ID where IMAGE_TYPE like 'WHOLE';
+
+create or replace view SURVEY_IMAGE_RESULT as select SURVEY_ID,IMAGE_ID,count(*) cnt,max(IMAGE_DESC) DESC from SERVEY_IMAGE  group by SURVEY_ID,IMAGE_ID order by cnt desc;
