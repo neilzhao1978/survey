990aed8cd51b682b47997aa3b3a18921cdda5c58
xavieryuan <xavieryuan@gmail.com>
2018-01-23 22:16:21 +0800

    重新设置gitignore

--- target/classes/init.sql
+++
@@ -1,6 +0,0 @@
-select * from ANSWER;
---insert into creator(email,pwd) values('niel@123.com','123')
-
---create or replace view SERVEY_IMAGE as SELECT a.SURVEY_ID,a.ANSWER_ID,im.IMAGE_ID,im.IMAGE_NAME,im.IMAGE_DESC FROM ANSWER a left join ANSWER_IMAGE i  on a.ANSWER_ID =i.ANSWER_ID  left join IMAGE im on i.Image_id = im.IMAGE_ID where IMAGE_TYPE like 'WHOLE';
-
---create or replace view SURVEY_IMAGE_RESULT as select SURVEY_ID,IMAGE_ID,count(*) cnt,max(IMAGE_DESC) DESC from SERVEY_IMAGE  group by SURVEY_ID,IMAGE_ID order by cnt desc;
