001c7671cf6985382122d9d465d7845909330fae
xavieryuan <xavieryuan@gmail.com>
2018-02-11 11:35:49 +0800

    Data 3d

--- static/common/js/DataVis3DRenderer.js
+++ static/common/js/DataVis3DRenderer.js
@@ -6,16 +6,20 @@
 const GET_PRODUCT_DATA_URL = host+"";
 
 class Product{
-    constructor(thumb_url){
+    constructor(_scene,thumb_url){
+        this.scene = _scene
         this.geometry = new THREE.PlaneGeometry(8, 5.4);
         this.material = new THREE.MeshBasicMaterial({
             color: 0xCCCCCC,
             side: THREE.DoubleSide
         });
         this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.up = new THREE.Vector3(0, 0, 1);
+        this.scene.add(this.mesh);
+
+
         this.thumb = new THREE.TextureLoader();
-        this.mesh.up = new THREE.Vector3(0, 0, 1)
-        this.loadThumbImage(thumb_url)
+        this.loadThumbImage(thumb_url);
     }
     loadThumbImage (url) {
         this.thumb.load(url,  (t)=> {
@@ -32,9 +36,15 @@ class Product{
     lookAt(v3){
         this.mesh.lookAt(v3)
     }
-    getMesh () {
-        return this.mesh;
-    };
+    addToScene(){
+        this.scene.add(this.mesh)
+    }
+    lightOn(){
+
+    }
+    lightOff(){
+
+    }
 }
 
 class DataVis3DRenderer{
@@ -43,10 +53,7 @@ class DataVis3DRenderer{
         let self = this;
         this.frameId = 0;
         //获取容器
-        let DOM_canvas = $("#"+DOM_ele).find("canvas").get(0)
-
-        this.productData = null;
-
+        let DOM_canvas = $("#"+DOM_ele).find("canvas").get(0);
         //创建THREEJS渲染器实例
         this.renderer = new THREE.WebGLRenderer({
             antialias: true,
@@ -60,6 +67,7 @@ class DataVis3DRenderer{
         this.camera.up = new THREE.Vector3(0, 0, 1);
         this.camera.position.set(100,100,100);
         this.camera.lookAt(this.center);
+        this.raycaster = new THREE.Raycaster();  
 
         //OrbitControls
         this.orbit = new THREE.OrbitControls( this.camera, this.renderer.domElement );
@@ -73,11 +81,34 @@ class DataVis3DRenderer{
 	    let ambient = new THREE.AmbientLight(color.getHex());
         this.scene.add(ambient);
         
+        //产品数据
+        this.productData = null;
+        this.products = []
         //加载产品
         this.loadProduct();
         
-        
         this.render()
+
+        //绑定鼠标事件处理
+        $(DOM_canvas).on("click",(evt)=>{
+            let mousePos = new THREE.Vector2();
+            mousePos.x = ( evt.clientX / window.innerWidth ) * 2 - 1;
+            mousePos.y = - ( evt.clientY / window.innerHeight ) * 2 + 1;
+            //update picking ray
+            this.raycaster.setFromCamera( mousePos, this.camera );
+            let intersects = this.raycaster.intersectObjects( scene.children );
+            //found a target
+            if (intersects.length>=1){
+                let target = intersects[ 0 ].object;
+                for(let i = 0;i<this.products.length;i++){
+                    this.products[i].lightOff();
+                    if (target === this.products[i].mesh){
+                        this.products[i].lightOn();
+                    }
+                }
+            }
+
+        })
 
     }
     render(){
@@ -110,23 +141,17 @@ class DataVis3DRenderer{
     loadProduct(){
 
         for (let i=0;i<100;i++){
-            let p = new Product("http://design-pinwall.qiniudn.com/5081/1517128537264.png?imageMogr2/thumbnail/200x200")
-            this.scene.add(p.getMesh())
+            let p = new Product(this.scene,"http://design-pinwall.qiniudn.com/5081/1517128537264.png?imageMogr2/thumbnail/200x200")
+            
             let x = 50-Math.random()*100;
             let y = 50-Math.random()*100;
             let z = 50-Math.random()*100;
             p.setPos(new THREE.Vector3( x, y, z ));
             p.lookAt(new THREE.Vector3( 0, 0, z ));
-        }
 
+            this.products.push(p);
+        }
         
-        
-        // let geometry = new THREE.SphereGeometry(1, 32, 32);
-	    // let material = new THREE.MeshPhongMaterial({
-        //     color: new THREE.Color('grey')
-        // });
-        // let sphere = new THREE.Mesh(geometry, material);
-        // this.scene.add(sphere);
     }
     drawBackground(url){
         let img_url = url || DEFAULT_FL_BG_URL;
